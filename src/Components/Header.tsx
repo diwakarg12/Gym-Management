a5e38ecae09416dd7f5dd58b8ffbec35784d3e77
Diwakar Giri <diwakargiri23@gmail.com>
2024-01-31 19:03:57 +0530

    First Commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="Logo" />
+      </div>
+      <div>
+        <ul>
+          <li className="bg-black">Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
